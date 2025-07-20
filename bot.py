180102450f55c7c6827241d8d8b7d188082cbda9
MUHAMMED ADNAN <73103641+mr-adnan-adu@users.noreply.github.com>
2025-07-20 15:57:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, KEEP_ALIVE_URL, DEFAULT_AUTH_CHANNELS
+from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from Script import script
